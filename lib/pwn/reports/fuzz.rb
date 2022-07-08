6fc615e0baed91351f706ee3c5c3be8e4bd8da9b
Jacob Hoopes <jake.hoopes@gmail.com>
2022-07-08 16:26:47 -0600

    PWN::Reports::* - bugfix in JSON.pretty_generate

--- lib/pwn/reports/fuzz.rb
+++ lib/pwn/reports/fuzz.rb
@@ -26,9 +26,7 @@ module PWN
         # JSON object Completion
         File.open("#{dir_path}/pwn_fuzz_net_app_proto.json", "w:#{char_encoding}") do |f|
           f.print(
-            JSON.pretty_generate(
-              results_hash.to_json.force_encoding(char_encoding)
-            )
+            JSON.pretty_generate(results_hash).force_encoding(char_encoding)
           )
         end
 
