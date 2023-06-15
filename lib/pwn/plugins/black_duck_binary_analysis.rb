cf9201e8e510c680b84c931faee663598142e481
Jacob Hoopes <jake.hoopes@gmail.com>
2023-06-15 10:52:24 -0600

    PWN::Plugins::BlackDuckBinaryAnalysis module - add a lot more API-related methods #bugfix

--- lib/pwn/plugins/black_duck_binary_analysis.rb
+++ lib/pwn/plugins/black_duck_binary_analysis.rb
@@ -76,7 +76,7 @@ module PWN
               authorization: "Bearer #{token}"
             }
             response = rest_client.execute(
-              method: http2_method,
+              method: http_method,
               url: "#{base_bd_bin_analysis_api_uri}/#{rest_call}",
               headers: headers,
               payload: http_body.to_json,
--- lib/pwn/version.rb
+++ lib/pwn/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module PWN
-  VERSION = '0.4.730'
+  VERSION = '0.4.731'
 end
