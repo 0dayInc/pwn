8dac465ec7a7a689cd5c2b66ecd6734bfcd2e4ea
Jacob Hoopes <jake.hoopes@gmail.com>
2023-10-11 09:39:17 -0600

    pwn_sast Driver - Incorporate new SAST module PWN::SAST::TypeScriptTypeJuggling #add_spec

---
+++ spec/lib/pwn/sast/type_script_type_juggling_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe PWN::SAST::TypeScriptTypeJuggling do
+  it 'scan method should exist' do
+    scan_response = PWN::SAST::TypeScriptTypeJuggling
+    expect(scan_response).to respond_to :scan
+  end
+
+  it 'should display information for security_references' do
+    security_references_response = PWN::SAST::TypeScriptTypeJuggling
+    expect(security_references_response).to respond_to :security_references
+  end
+
+  it 'should display information for authors' do
+    authors_response = PWN::SAST::TypeScriptTypeJuggling
+    expect(authors_response).to respond_to :authors
+  end
+
+  it 'should display information for existing help method' do
+    help_response = PWN::SAST::TypeScriptTypeJuggling
+    expect(help_response).to respond_to :help
+  end
+end
