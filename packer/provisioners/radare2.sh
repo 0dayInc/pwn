3c97de9fc62c59453390f45a1a44e5a7e992a8f5
Jacob Hoopes <jake.hoopes@gmail.com>
2025-04-30 15:29:27 -0600

    Packer provisioner Radare2 - install decompiler and AI plugins && add r2 alias to aliases.sh

--- packer/provisioners/radare2.sh
+++ packer/provisioners/radare2.sh
@@ -1,4 +1,27 @@
 #!/bin/bash --login
 source /etc/profile.d/globals.sh
 
-sudo /bin/bash --login -c 'cd /opt && git clone https://github.com/radareorgg/radare2 && ./radare2/sys/install.sh'
+# This is the preferred method of installation
+# to support radare2 plugin installation (e.g. r2ghidra)
+sudo apt install -y capstone-tool meson
+cd /opt && sudo git clone https://github.com/radareorg/radare2 && chown -R $USER:$USER radare2
+cd radare2 && ./sys/install.sh
+
+r2 -qq -c 'r2pm -U' /bin/id
+r2 -qq -c 'r2pm -ci decai' /bin/id
+r2 -qq -c 'r2pm -ci r2ai-plugin' /bin/id
+r2 -qq -c 'r2pm -ci r2dec' /bin/id
+r2 -qq -c 'r2pm -ci r2ghidra-sleigh' /bin/id
+r2 -qq -c 'r2pm -ci r2ghidra' /bin/id
+r2 -qq -c 'r2pm -ci r2frida' /bin/id
+
+r2pm -U
+r2pm -ci decai
+r2pm -ci r2ai-plugin
+r2pm -ci r2dec
+r2pm -ci r2ghidra-sleigh
+r2pm -ci r2ghidra
+r2pm -ci r2frida
+
+mkdir -p ~/.local/share/radare2/r2panels
+cp $PWN_ROOT/third-party/r2-pwn-layout ~/.local/share/radare2/r2panels/
