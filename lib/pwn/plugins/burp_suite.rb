857cab9cb7429c58b59a08416e9fab590be56b8f
Jacob Hoopes <jake.hoopes@gmail.com>
2025-08-04 11:50:43 -0600

    PWN::Plugins::BurpSuite module - implement a #spider method and include this feature as an option within the pwn_burp_suite_pro_active_scan Driver. #enhancements

--- lib/pwn/plugins/burp_suite.rb
+++ lib/pwn/plugins/burp_suite.rb
@@ -193,9 +193,15 @@ module PWN
           spider_status_resp = rest_browser.get("http://#{pwn_burp_api}/spider/#{spider_id}")
           spider_status_json = JSON.parse(spider_status_resp, symbolize_names: true)
           spider_status = spider_status_json[:status]
-          break if spider_status == 'finished'
-
-          sleep 3
+          case spider_status
+          when 'queued', 'running'
+            sleep 3
+          when 'failed', 'finished'
+            break
+          else
+            puts "Unknown spider status detected: #{spider_status}"
+            break
+          end
         end
 
         spider_json.merge!(spider_status_json)
--- lib/pwn/version.rb
+++ lib/pwn/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module PWN
-  VERSION = '0.5.337'
+  VERSION = '0.5.338'
 end
