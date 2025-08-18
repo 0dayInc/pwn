29f8925be76323ad55bf197e546e7b172dbed616
Jacob Hoopes <jake.hoopes@gmail.com>
2025-08-18 16:49:10 -0600

    Add new namespace, `PWN::AI` and move PWN::Plugins::OpenAI and PWN::Plugins::Ollama modules to the new namespace.  Also new initial commit for `PWN::AI::Grok` module.

--- spec/lib/pwn/ai/grok_spec.rb
+++ spec/lib/pwn/ai/grok_spec.rb
@@ -2,14 +2,14 @@
 
 require 'spec_helper'
 
-describe PWN::Plugins::Ollama do
+describe PWN::AI::Grok do
   it 'should display information for authors' do
-    authors_response = PWN::Plugins::Ollama
+    authors_response = PWN::AI::Grok
     expect(authors_response).to respond_to :authors
   end
 
   it 'should display information for existing help method' do
-    help_response = PWN::Plugins::Ollama
+    help_response = PWN::AI::Grok
     expect(help_response).to respond_to :help
   end
 end
