cd06863b929aba16428158740dc4ae37897b9377
Jacob Hoopes <jake.hoopes@gmail.com>
2025-09-08 15:47:38 -0600

    pwn_burp_*, pwn_zaproxy*, PWN::Plugins::BurpSuite, && PWN::Plugins::Zaproxy - sync dev patterns as closely as possible - iteration 1

--- spec/lib/pwn/plugins/zaproxy_spec.rb
+++ spec/lib/pwn/plugins/zaproxy_spec.rb
@@ -2,14 +2,14 @@
 
 require 'spec_helper'
 
-describe PWN::Plugins::OwaspZap do
+describe PWN::Plugins::Zaproxy do
   it 'should display information for authors' do
-    authors_response = PWN::Plugins::OwaspZap
+    authors_response = PWN::Plugins::Zaproxy
     expect(authors_response).to respond_to :authors
   end
 
   it 'should display information for existing help method' do
-    help_response = PWN::Plugins::OwaspZap
+    help_response = PWN::Plugins::Zaproxy
     expect(help_response).to respond_to :help
   end
 end
